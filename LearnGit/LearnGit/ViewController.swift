3e705591491991c67eafbf0e4ca6fb603a618670
alicerose <s3634707@rmit.edu.vn>
2019-12-28 00:12:49 +0700

    init project

---
+++ LearnGit/LearnGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnGit
+//
+//  Created by Ma Alice on 12/28/19.
+//  Copyright © 2019 Ma Alice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
